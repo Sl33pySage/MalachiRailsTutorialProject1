cae3fdbba2ebc639c6860a91a30bab0c9a1afcae
Sl33pySage <jasonwolf82@hotmail.com>
2025-04-09 04:48:02 -0500

    On Part1 time@21:24

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_09_091334) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_09_093308) do
   create_table "blogs", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", null: false
